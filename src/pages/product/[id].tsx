19d08b096f26084ae1a0fb072954e035c730eea8
Sanarielsen <sanarielsen.dev@gmail.com>
2025-09-10 20:58:11 -0300

    feat(ISP-04): create pages to product and checkout
    Create the pages can be access the details of product, checkout, success
    path when you buy a product. Also create a button to redirect to the
    checkout page from the product details page.
    
    Resolves: ISP-04-01, ISP-04-02

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,116 @@
-import { useRouter } from 'next/router'
+import { stripe } from '@/src/lib/stripe';
+import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product';
+import axios from 'axios';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Stripe from 'stripe';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string,
+    defaultPriceId: string,
+  }
+}
+
+export default function Product( { product }: ProductProps ) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+      alert('Falha ao redirecionar ao checkout')
+    }
+  }
+
+  const { isFallback } = useRouter();
+
+  if (isFallback)
+    return <h1>Loading...</h1> 
 
   return (
-    <p>Product: {JSON.stringify(query)}</p>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+        </ImageContainer>  
+
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+
+          <button 
+            onClick={handleBuyProduct}
+            disabled={isCreatingCheckoutSession}
+          >Comprar agora</button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
+    
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    //Coloca os id mais acessados;
+    paths: [
+      { params: { id: "prod_T174U86l1LfNQc" } },
+    ],
+    fallback: true
+  }
+} 
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+        defaultPriceId: price.id
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
 }
