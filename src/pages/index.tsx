11e4ff7f799a5577ba147fc354ceb0cd4c47135b
Sanarielsen <sanarielsen.dev@gmail.com>
2025-09-10 20:58:11 -0300

    feat(ISP-04): create pages to product and checkout
    Create the pages can be access the details of product, checkout, success
    path when you buy a product. Also create a button to redirect to the
    checkout page from the product details page.
    
    Resolves: ISP-04-01, ISP-04-02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Head from 'next/head'
 import { HomeContainer, Product } from "../styles/pages/home";
+import Link from 'next/link'
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
@@ -26,29 +28,34 @@ export default function Home( {products}: HomeProps ) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-
-      {products.map( product => {
-        return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image 
-              src={product.imageUrl} 
-              width={520} 
-              height={480}
-              alt=""
-             />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      } )}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+    
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map( product => {
+          return (
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+              <Product className="keen-slider__slide">
+                <Image 
+                  src={product.imageUrl} 
+                  width={520} 
+                  height={480}
+                  alt=""
+                />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        } )}
+      </HomeContainer>
+    </>
   );
 }
-
-// GetServerSideProps is like
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
