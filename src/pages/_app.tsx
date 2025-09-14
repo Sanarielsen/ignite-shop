3f7bb2357ed1b90d2045fd7bc524f5de5e41ca08
Sanarielsen <sanarielsen.dev@gmail.com>
2025-09-13 23:01:22 -0300

    feat(ISP-05-01): create button cart on header
    Create button to call visual cart you have to buy if you proceed to
    checkout.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,26 @@ import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
 import logoImg from '../assets/logo.svg';
-import { Container, Header } from "../styles/pages/app";
+import cartImg from '../assets/CartIcon.svg';
+import { CartIcon, Container, Header } from "../styles/pages/app";
 
 import Image from 'next/image'
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {    
+
+  function handleClickOpenCart() {
+    console.log("handleClickOpenCart() -> Executed")
+  }
+
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" width={130} height={52}/>
+        <CartIcon onClick={handleClickOpenCart}>
+          <Image src={cartImg} alt="" width={24} height={24}/>
+        </CartIcon>
       </Header>
       <Component {...pageProps} />
     </Container>
